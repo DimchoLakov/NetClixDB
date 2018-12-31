410890ab59f9ebee455f056bafcdbc1692101297
whywherewhat <mdsektor@gmail.com>
2018-12-31 07:31:08 +0000

    Index page displays movies

--- src/Web/NetPhlixDB.Web/Controllers/HomeController.cs
+++ src/Web/NetPhlixDB.Web/Controllers/HomeController.cs
@@ -1,18 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebSockets.Internal;
+using NetPhlixDB.Services.Contracts;
+using NetPhlixDB.Web.Common;
 using NetPhlixDB.Web.Models;
 
 namespace NetPhlixDB.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IMovieService _movieService;
+
+        public HomeController(IMovieService movieService)
+        {
+            this._movieService = movieService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var indexMovies = this._movieService.GetAllIndexMovies().Take(NetConstants.IndexMoviesCount).ToList();
+
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return View("IndexLoggedIn", indexMovies);
+            }
+            
+            return View(indexMovies);
         }
 
         public IActionResult Privacy()
