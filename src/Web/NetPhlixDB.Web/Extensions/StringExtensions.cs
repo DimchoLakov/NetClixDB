f4304a2446087bfd2f40e4aedc42759f99259107
whywherewhat <mdsektor@gmail.com>
2019-01-08 09:26:00 +0000

    Added String Extensions. Youtube link converter

---
+++ src/Web/NetPhlixDB.Web/Extensions/StringExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NetPhlixDB.Web.Extensions
+{
+    public static class StringExtensions
+    {
+        static readonly Regex YoutubeVideoRegex = new Regex(@"youtu(?:\.be|be\.com)/(?:(.*)v(/|=)|(.*/)?)([a-zA-Z0-9-_]+)", RegexOptions.IgnoreCase);
+        static readonly Regex VimeoVideoRegex = new Regex(@"vimeo\.com/(?:.*#|.*/videos/)?([0-9]+)", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+
+        // Use as
+        // string youtubeLink = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        // var embedCode = youtubeLink.UrlToEmbedCode();
+        public static string UrlToEmbedCode(this string url)
+        {
+            if (!string.IsNullOrEmpty(url))
+            {
+                var youtubeMatch = YoutubeVideoRegex.Match(url);
+
+                if (youtubeMatch.Success)
+                {
+                    return getYoutubeEmbedCode(youtubeMatch.Groups[youtubeMatch.Groups.Count - 1].Value);
+                }
+
+                var vimeoMatch = VimeoVideoRegex.Match(url);
+                if (vimeoMatch.Success)
+                {
+                    return getVimeoEmbedCode(vimeoMatch.Groups[1].Value);
+                }
+
+            }
+
+            return null;
+        }
+
+        const string youtubeEmbedFormat = "https://www.youtube.com/embed/{0}";
+
+        private static string getYoutubeEmbedCode(string youtubeId)
+        {
+            return string.Format(youtubeEmbedFormat, youtubeId);
+        }
+
+        const string vimeoEmbedFormat = "https://player.vimeo.com/video/{0}";
+        private static string getVimeoEmbedCode(string vimeoId)
+        {
+            return string.Format(vimeoEmbedFormat, vimeoId);
+        }
+    }
+}
