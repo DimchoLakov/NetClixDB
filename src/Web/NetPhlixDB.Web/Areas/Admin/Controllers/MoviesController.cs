0626492fcc7240fa4c1adcdc025ca1fa80fd5de5
whywherewhat <mdsektor@gmail.com>
2019-01-08 09:25:27 +0000

    Added Admin Area

---
+++ src/Web/NetPhlixDB.Web/Areas/Admin/Controllers/MoviesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NetPhlixDb.Data.ViewModels.Binding.Movies;
+using NetPhlixDB.Data;
+using NetPhlixDB.Data.Models;
+using NetPhlixDB.Web.Extensions;
+
+namespace NetPhlixDB.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MoviesController : Controller
+    {
+        private readonly NetPhlixDbContext _context;
+        private readonly IMapper _mapper;
+
+        public MoviesController(NetPhlixDbContext context, IMapper mapper)
+        {
+            _context = context;
+            this._mapper = mapper;
+        }
+
+        // GET: Admin/Movies
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Movies.ToListAsync());
+        }
+
+        // GET: Admin/Movies/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: Admin/Movies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateMovieViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var trailer = viewModel.Trailer.UrlToEmbedCode();
+                if (trailer == null)
+                {
+                    return View(viewModel);
+                }
+                var movie = this._mapper.Map<CreateMovieViewModel, Movie>(viewModel);
+                movie.Trailer = trailer;
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(viewModel);
+        }
+
+        // GET: Admin/Movies/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Admin/Movies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Title,Storyline,Language,Poster,Trailer,DateReleased,ProductionCost,Duration,Rating,MovieType,Id")] Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Admin/Movies/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Admin/Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            _context.Movies.Remove(movie);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(string id)
+        {
+            return _context.Movies.Any(e => e.Id == id);
+        }
+    }
+}
