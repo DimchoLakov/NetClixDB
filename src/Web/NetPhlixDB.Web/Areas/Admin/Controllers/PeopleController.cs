9dbfcc44fd1b4079b1cb6818f3ffb9be14f2ca15
whywherewhat <mdsektor@gmail.com>
2019-01-10 08:57:04 +0000

    Introduced PeopleController

---
+++ src/Web/NetPhlixDB.Web/Areas/Admin/Controllers/PeopleController.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetPhlixDb.Data.ViewModels.Admin.People;
+using NetPhlixDB.Data;
+using NetPhlixDB.Data.Models;
+
+namespace NetPhlixDB.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PeopleController : Controller
+    {
+        private readonly NetPhlixDbContext _context;
+        private readonly IMapper _mapper;
+
+        public PeopleController(NetPhlixDbContext context, IMapper mapper)
+        {
+            this._context = context;
+            this._mapper = mapper;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var people = await _context.People.ToListAsync();
+            var peopleViewModel = this._mapper.Map<IEnumerable<Person>, IEnumerable<IndexPersonViewModel>>(people);
+
+            return View(peopleViewModel);
+        }
+        
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.People
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Create(CreatePersonViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var person = this._mapper.Map<CreatePersonViewModel, Person>(viewModel);
+                _context.Add(person);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(viewModel);
+        }
+
+        // GET: Admin/People/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.People.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            var personViewModel = this._mapper.Map<Person, EditPersonViewModel>(person);
+            return View(personViewModel);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, EditPersonViewModel viewModel)
+        {
+            if (id != viewModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var person = this._mapper.Map<EditPersonViewModel, Person>(viewModel);
+                try
+                {
+                    _context.Update(person);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PersonExists(person.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(viewModel);
+        }
+        
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.People
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+        
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var person = await _context.People.FindAsync(id);
+            _context.People.Remove(person);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PersonExists(string id)
+        {
+            return _context.People.Any(e => e.Id == id);
+        }
+    }
+}
