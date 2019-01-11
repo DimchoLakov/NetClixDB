e03d5ed7ffd09866915ef4c8a9c88e7d4eeafb74
whywherewhat <mdsektor@gmail.com>
2019-01-11 06:22:26 +0000

    Added Remove Favorite Movie VM

---
+++ src/Data/NetPhlixDb.Data.ViewModels/People/RemoveFavMovieViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetPhlixDb.Data.ViewModels.People
+{
+    public class RemoveFavMovieViewModel
+    {
+        [Required]
+        public string Id { get; set; }
+    }
+}
