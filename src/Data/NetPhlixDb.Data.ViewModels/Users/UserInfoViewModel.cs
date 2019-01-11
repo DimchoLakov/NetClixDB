2690cdb26d742d462b5866c19dbb50b85cebe23b
whywherewhat <mdsektor@gmail.com>
2019-01-11 06:52:58 +0000

    Added User Info VM

---
+++ src/Data/NetPhlixDb.Data.ViewModels/Users/UserInfoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NetPhlixDb.Data.ViewModels.Users
+{
+    public class UserInfoViewModel
+    {
+        public string UserName { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string Avatar { get; set; }
+    }
+}
