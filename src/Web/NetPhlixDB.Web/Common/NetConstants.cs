7b28044336744fd2f6f8aa936d5308ece8c0da00
whywherewhat <mdsektor@gmail.com>
2019-01-11 06:34:57 +0000

    Page size changed to 4

--- src/Web/NetPhlixDB.Web/Common/NetConstants.cs
+++ src/Web/NetPhlixDB.Web/Common/NetConstants.cs
@@ -4,7 +4,7 @@
     {
         public const int IndexMoviesCount = 4;
 
-        public const int PageSize = 10;
+        public const int PageSize = 4;
 
         public const int ZeroResult = 0;
     }
