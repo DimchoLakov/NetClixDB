885109477258fb0b261eb46caa66d70cf616b0f6
whywherewhat <mdsektor@gmail.com>
2019-01-11 06:22:12 +0000

    Added Create Event VM

---
+++ src/Data/NetPhlixDb.Data.ViewModels/Events/CreateEventViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetPhlixDb.Data.ViewModels.Events
+{
+    public class CreateEventViewModel
+    {
+        [Required]
+        [StringLength(maximumLength: 64, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Title")]
+        [DataType(DataType.Text)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 3000, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Info")]
+        [DataType(DataType.Text)]
+        public string Info { get; set; }
+
+        [Required]
+        [Display(Name = "Picture")]
+        [DataType(DataType.Url)]
+        public string Picture { get; set; }
+
+        [Required]
+        [Display(Name = "Date")]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+    }
+}
