6b0b3cfd3db7aec90eb99b0354f242259bd8d11a
whywherewhat <mdsektor@gmail.com>
2019-01-26 04:33:42 +0000

    Added Profile Constants

---
+++ src/Services/Mapping/NetPhlixDB.Services.Mapping/Constants/ProfileConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace NetPhlixDB.Services.Mapping.Constants
+{
+    public class ProfileConstants
+    {
+        public const string FullDateFormat = "MMM/dd/yyyy";
+
+        public const string YearDateFormat = "yyyy";
+    }
+}
