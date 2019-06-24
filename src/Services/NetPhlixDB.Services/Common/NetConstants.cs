e93fc5dfa935f33aac62da5dc5b1056825f6662f
dimcho <dimcho@banntech.co.uk>
2019-06-24 02:20:02 +0100

    Move NetConstants

---
+++ src/Services/NetPhlixDB.Services/Common/NetConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace NetPhlixDB.Services.Common
+{
+    public class NetConstants
+    {
+        public const int LoggedOutMoviesCount = 4;
+
+        public const int DefaultPageSize = 8;
+
+        public const int ZeroResult = 0;
+    }
+}
