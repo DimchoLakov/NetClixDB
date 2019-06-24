cf45764eea5d6e9a401a80aa0b1e2ca40cae4eb6
dimcho <dimcho@banntech.co.uk>
2019-06-24 02:18:45 +0100

    Add Pagination View Model

---
+++ src/Data/NetPhlixDb.Data.ViewModels/Movies/PaginationMoviesViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace NetPhlixDb.Data.ViewModels.Movies
+{
+    public class PaginationMoviesViewModel
+    {
+        public PaginationMoviesViewModel()
+        {
+            this.IndexMovieViewModels = new List<IndexMovieViewModel>();
+        }
+
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+
+        [Display(Name = "Title")]
+        public string Search { get; set; }
+
+        public string Genre { get; set; }
+
+        public List<SelectListItem> Genres { get; set; }
+        
+        public IList<IndexMovieViewModel> IndexMovieViewModels { get; set; }
+    }
+}
