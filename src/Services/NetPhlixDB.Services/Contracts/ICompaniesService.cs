f60b18fcb55d0feb94a80ccc391099eaf1fa22cf
DimchoLakov <mdsektor@gmail.com>
2019-07-25 22:50:57 +0100

    Add services

--- src/Services/NetPhlixDB.Services/Contracts/ICompaniesService.cs
+++ src/Services/NetPhlixDB.Services/Contracts/ICompaniesService.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using NetPhlixDb.Data.ViewModels.Admin.Companies;
 using NetPhlixDb.Data.ViewModels.Companies;
 
 namespace NetPhlixDB.Services.Contracts
 {
     public interface ICompaniesService
     {
-        Task<CompanyViewModel> GetCompanyDetails(string id);
+        Task<bool> CompanyExists(string id);
+
+        Task<int?> Delete(string id);
+
+        Task<int?> Update(EditDeleteDetailsCompanyViewModel viewModel);
+
+        Task<int> Create(CreateCompanyViewModel viewModel);
+
+        Task<EditDeleteDetailsCompanyViewModel> GetByIdAdmin(string id);
+
+        Task<CompanyViewModel> GetById(string id);
 
         Task<IEnumerable<CompanyShortViewModel>> GetAll();
     }
---
+++ src/Services/NetPhlixDB.Services/GenresService.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using NetPhlixDb.Data.ViewModels.Admin.Genres;
+using NetPhlixDB.Data;
+using NetPhlixDB.Services.Contracts;
+
+namespace NetPhlixDB.Services
+{
+    public class GenresService : IGenresService
+    {
+        private readonly NetPhlixDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GenresService(NetPhlixDbContext dbContext, IMapper mapper)
+        {
+            this._dbContext = dbContext;
+            this._mapper = mapper;
+        }
+
+        public async Task<IEnumerable<IndexGenreViewModel>> GetAllAsync()
+        {
+            var allGenres = await this._dbContext.Genres.ToListAsync();
+            var genres = this._mapper.Map<IEnumerable<IndexGenreViewModel>>(allGenres);
+            return genres;
+        }
+
+        public async Task<EditDeleteDetailsGenreViewModel> GetByIdAsync(string id)
+        {
+            var genre = await this._dbContext.Genres.FindAsync(id);
+            var genreViewModel = this._mapper.Map<EditDeleteDetailsGenreViewModel>(genre);
+            return genreViewModel;
+        }
+
+        public Task<int?> CreateAsync(CreateGenreViewModel genreViewModel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<int?> EditAsync(EditDeleteDetailsGenreViewModel viewModel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<int?> DeleteAsync(EditDeleteDetailsGenreViewModel viewModel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> GenreExistsAsync(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
