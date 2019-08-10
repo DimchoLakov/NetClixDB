5175ee2ca8e0c326b59180a4d9f5e6481d43736f
dimcho <dimcho@banntech.co.uk>
2019-08-10 15:53:08 +0100

    Add Email configuration

---
+++ src/Web/NetPhlixDB.Web/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace NetPhlixDB.Web.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string SendGridUser { get; set; }
+
+        public string SendGridKey { get; set; }
+    }
+}
